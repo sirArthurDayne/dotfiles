6a304ea2443bfb4d8ed72a732d7633b3d010e61d
sirArthurDayne <xavierivan2000@hotmail.com>
2021-12-21 01:21:24 -0500

    add windows terminal settings.json file

--- .config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ .config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -29,25 +29,28 @@ $dev_path = "D:\Dev"
 $visual_proyects_path = "D:\Dev\Proyects\VisualProyects"
 $web_proyects_path = "D:\Dev\Proyects\webProyects"
 $dotfiles_path = "D:\Dev\Proyects\githubDownloads\dotfiles"
+$windowsTerminalConfig_path = "C:\Users\xavie\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 #My functions
 function psprofile { nvim $PROFILE }
 function Get-Dotfiles { Set-Location $dotfiles_path }
+function Set-TerminalSettings { nvim $windowsTerminalConfig_path }
 function Nvimconfig { nvim $nvimconfig_path}
 function Windevhome { Set-Location $dev_path}
 function windvisual { Set-Location $visual_proyects_path }
 function Webpath() { Set-Location $web_proyects_path }
 function Windu() { Set-Location 'D:\vainas de la U\' }
-function Glog() { git log --oneline --decorate --graph }
+function Get-Glog() { git log --oneline --decorate --graph }
 function Gstatus() { git status -s }
 #My aliases
 Set-Alias -Name touch -Value New-Item
 Set-Alias -Name cl -Value Clear-Host
 Set-Alias -Name vi -Value nvim
 Set-Alias -Name ll -Value ls
-Set-Alias -Name glog -Value Glog
+Set-Alias -Name glog -Value Get-Glog
 Set-Alias -Name gs -Value Gstatus
 Set-Alias -Name dev -Value Windevhome
 Set-Alias -Name uni -Value Windu
 Set-Alias -Name viconf -Value Nvimconfig
 Set-Alias -Name webdev -Value Webpath
 Set-Alias -Name dotfiles -Value Get-Dotfiles
+Set-Alias -Name termconf -Value Set-TerminalSettings
