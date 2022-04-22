8f2b8cbe8e5abfb51f168c7532856b09b37b9e8c
sirArthurDayne <xavierivan2000@hotmail.com>
2022-04-22 15:05:24 -0400

    updates path and main.yml

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env zsh
-mkdir -p ~/.config/nvim/plugin
-mkdir -p ~/.config/nvim/after/plugin
-mkdir -p ~/.config/nvim/lua
+#!/bin/bash
 
-# remove the nvim folder and link every .vim or .lua file
-for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
-    rm -rf ~/.config/nvim/$f
-    ln -s ~/personal/dotfiles/$f ~/.config/nvim/$f
+pushd $DOTFILES
+for folder in "${STOW_FOLDERS[@]}"
+do
+	echo $folder
 done
+popd
