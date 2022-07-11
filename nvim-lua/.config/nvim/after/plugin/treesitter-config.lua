1f826578a494ac101180a6c519f5dfc1b2a9fca1
xavier lamela <xavierivan2000@hotmail.com>
2022-07-11 15:02:16 -0500

    add golang and python TS parser

--- nvim-lua/.config/nvim/after/plugin/treesitter-config.lua
+++ nvim-lua/.config/nvim/after/plugin/treesitter-config.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = {"html", "javascript", "lua"},
+    ensure_installed = { "html", "javascript", "lua", "python", "go" },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false
