a5da24d3c98c9c537385c277bbcb0b8a6881f736
xavier lamela <xavierivan2000@hotmail.com>
2023-03-25 17:59:09 -0500

    add null-ls format code on cmd

--- nvim-lua/.config/nvim/lua/krosis/nulls-config/init.lua
+++ nvim-lua/.config/nvim/lua/krosis/nulls-config/init.lua
@@ -1,9 +1,13 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        -- null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.diagnostics.eslint,
-        -- null_ls.builtins.completion.spell,
-    },
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		-- null_ls.builtins.diagnostics.eslint,
+		-- null_ls.builtins.completion.spell,
+	},
 })
+
+vim.keymap.set("n", "<leader>vf", function()
+	return vim.lsp.buf.format({ async = true })
+end, {})
